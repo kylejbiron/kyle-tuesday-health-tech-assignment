79206bdd364624746858774a153c02d689e29e93
Kyle Biron <kylejbiron@gmail.com>
2024-09-30 14:04:28 -0400

    fix formatting on fact_hospice_provider_flattened

--- hospice_quality_metrics/models/analytics/fact_hospice_provider_flattened.sql
+++ hospice_quality_metrics/models/analytics/fact_hospice_provider_flattened.sql
@@ -1,54 +1,54 @@
 -- models/analytics/fact_hospice_provider_flattened.sql
 
-with GET_HIS_COMPOSITE_MEASURE as (
+with get_his_composite_measure as (
 
     select
-        CCN,
-        CMS_REGION,
-        MEASURE_CODE,
-        SCORE,
-        FOOTNOTE
+        ccn,
+        cms_region,
+        measure_code,
+        score,
+        footnote
     from {{ ref('stg_hospice_provider') }}
 
     -- H_008_01_ OBSERVED: Facility observed rate | Hospice and Palliative Care Composite Process Measure
-    where MEASURE_CODE = 'H_008_01_OBSERVED'
+    where measure_code = 'H_008_01_OBSERVED'
 
     --filter out records where score is not available
-    and SCORE != 'Not Available'
+    and score != 'Not Available'
 
 ),
 
-GET_HCI_OVERALL_SCORE as (
+get_hci_overall_score as (
 
     select
-        CCN,
-        CMS_REGION,
-        MEASURE_CODE,
-        SCORE,
-        FOOTNOTE
+        ccn,
+        cms_region,
+        measure_code,
+        score,
+        footnote
     from {{ ref('stg_hospice_provider') }}
 
     -- H_012_00_OBSERVED: Facility observed rate | Hospice Care Index Overall Score
-    where MEASURE_CODE = 'H_012_00_OBSERVED'
+    where measure_code = 'H_012_00_OBSERVED'
 
     --filter out records where score is not available
-    and SCORE != 'Not Available'
+    and score != 'Not Available'
 
 )
 
 select
 
-    HCI.CCN,
-    HCI.CMS_REGION,
-    HIS.SCORE as H_008_01_OBSERVED_SCORE,
-    HCI.SCORE as H_012_00_OBSERVED_SCORE,
-    HIS.FOOTNOTE as H_008_01_OBSERVED_FOOTNOTE,
-    HCI.FOOTNOTE as H_012_00_OBSERVED_FOOTNOTE
+    hci.ccn,
+    hci.cms_region,
+    his.score as h_008_01_observed_score,
+    hci.score as h_012_00_observed_score,
+    his.footnote as h_008_01_observed_footnote,
+    hci.footnote as h_012_00_observed_footnote
 
     -- HCI contains a larger set of CCNs
-from GET_HCI_OVERALL_SCORE as HCI
-inner join GET_HIS_COMPOSITE_MEASURE as HIS
-    on HCI.CCN = HIS.CCN
+from get_hci_overall_score as hci
+inner join get_his_composite_measure as his
+    on hci.ccn = his.ccn
 
 --note: 
 --footnote was brought into this fact table in case we needed to filter on it 
