df7aaa6d710e38b10ead653ba9f7a44d2d26134a
Kyle Biron <kylejbiron@gmail.com>
2024-10-01 17:16:46 -0400

    update etl for fact_hospice_provider_flattened in preparation for modeling

--- hospice_quality_metrics/models/analytics/fact_hospice_provider_flattened.sql
+++ hospice_quality_metrics/models/analytics/fact_hospice_provider_flattened.sql
@@ -18,10 +18,16 @@ select
     h_007_01_denominator,
     h_007_01_observed,
     h_008_01_denominator,
+
+    -- casting below cols as decimal for downstream modeling
     cast(h_008_01_observed as decimal(5, 1)) as h_008_01_observed,
+
     h_011_01_denominator,
     h_011_01_observed,
+
+    -- casting below cols as decimal for downstream modeling
     cast(h_012_00_observed as decimal(5, 1)) as h_012_00_observed,
+
     h_012_01_denominator,
     h_012_01_observed,
     h_012_01_percentile,
@@ -40,8 +46,11 @@ select
     h_012_06_denominator,
     h_012_06_observed,
     h_012_06_percentile,
-    h_012_07_denominator,
-    h_012_07_observed,
+
+    -- casting below cols as decimal for downstream modeling
+    cast(replace(h_012_07_denominator, ',', '') as decimal(15, 2)) as h_012_07_denominator,
+    cast(replace(h_012_07_observed, ',', '') as decimal(15, 2)) as h_012_07_observed,
+
     h_012_07_percentile,
     h_012_08_denominator,
     h_012_08_observed,
@@ -62,12 +71,19 @@ select
     care_provided_nursing_facility,
     care_provided_skilled_nursing,
     care_provided_other_locations,
-    pct_pts_w_cancer,
-    pct_pts_w_circ_heart_disease,
-    pct_pts_w_dementia,
-    pct_pts_w_resp_disease,
-    pct_pts_w_stroke,
-    pct_pts_w_other_conditions,
+
+    -- casting below cols as int for downstream modeling
+    cast(pct_pts_w_cancer as int) as pct_pts_w_cancer,
+    cast(pct_pts_w_circ_heart_disease as int) as pct_pts_w_circ_heart_disease,
+    cast(pct_pts_w_dementia as int) as pct_pts_w_dementia,
+    cast(pct_pts_w_resp_disease as int) as pct_pts_w_resp_disease,
+    cast(pct_pts_w_stroke as int) as pct_pts_w_stroke,
+    cast(pct_pts_w_other_conditions as int) as pct_pts_w_other_conditions,
+
     provided_home_care_and_other,
-    provided_home_care_only
+    provided_home_care_only,
+
+    -- round to the next highest integer to proxy length of stay
+    cast(ceil(cast(replace(h_012_07_observed, ',', '') as numeric) / 200) as int) as los_proxy
+
 from {{ ref('stg_hospice_provider_flattened') }}
